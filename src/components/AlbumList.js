0551adcdc16a6050c5f044bdd3c7ae3936448626
MikeH <mike123.henry@gmail.com>
2018-02-24 09:45:16 -0600

    added state to pass data around

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,10 +3,10 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
-  //componentWillMount = () => {
+  state = { albums: [] };
      componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response.data));
+      .then(response => this.setState({ albums: response.data }));
 //    fetch('https://rallycoding.herokuapp.com/api/music_albums', { method: 'GET' })
 //    .then((response) => response.json())
 //    .then((responseData) => console.log(responseData))
@@ -15,6 +15,7 @@ class AlbumList extends Component {
 //    });
   }
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>Album List xfcx!!!</Text>
