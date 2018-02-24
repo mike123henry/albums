e7fc906678b2debff69f5d0bcff8516c17a3d4f0
MikeH <mike123.henry@gmail.com>
2018-02-24 08:58:36 -0600

    added https -get- network call with axios packge and native fetch call - lots of debuggit statusgit status

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
-  componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+  //componentWillMount = () => {
+     componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response.data));
+//    fetch('https://rallycoding.herokuapp.com/api/music_albums', { method: 'GET' })
+//    .then((response) => response.json())
+//    .then((responseData) => console.log(responseData))
+//    .catch((error) => {
+//      console.error(error);
+//    });
   }
   render() {
     return (
       <View>
-        <Text>Album List xfx!!!</Text>
+        <Text>Album List xfcx!!!</Text>
       </View>
     );
   }
